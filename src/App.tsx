b88e892954d30550bbf272126d4bb74a5210d9a6
K-AMeus <karl.andreas@meus.ee>
2025-03-13 20:37:03 +0200

    add initial frontend functionality

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import UseCaseUploader from "./components/UseCaseUploader.tsx";
+
+const App = () => {
+    return(
+        <div>
+            <UseCaseUploader/>
+        </div>
+    )
+}
+
+export default App;
